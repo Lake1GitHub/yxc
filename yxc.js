5ec1561d8a7bc2c499fcbde31e414ee1a22f9c7b
Lake1GitHub <1695064014@qq.com>
2017-11-06 11:13:26 +0800

    update

---
+++ yxc.js
@@ -0,0 +1,86 @@
+var yxc = {
+  self: this,
+  eleIsExist: function(ele){
+    if(!ele){
+      throw('ele is valid');
+    }
+  },
+  $: function(ele){
+    this.eleIsExist(ele);
+    return document.querySelector(ele);
+  },
+  addEvent: function(ele, handle, fn){
+    this.eleIsExist(ele);
+    if(ele.addEventListener){
+      ele.addEventListener(handle, fn);
+    } else if(ele.attachEvent){
+      ele.attachEvent('on'+handle, fn);
+    } else {
+      ele[handle] = fn;
+    }
+  },
+  window: function(){
+    var w = window;
+    var width = w.innerWidth;
+    var height = w.innerHeight;
+    var outWidth = w.outerWidth;
+    var outHeight = w.outerHeight;
+    var inWidth = width;
+    var inHeight = height;
+    return{
+      width: width,
+      height: height,
+      outHeight: outHeight,
+      outWidth: outWidth,
+      inWidth: inWidth,
+      inHeight: inHeight
+    };
+
+  },
+  offset: function(ele){
+    var box = ele.getBoundingClientRect();
+    var w = window;
+    var d = w.document;
+    var winHeight, winWidth;
+    var width = box.width || (box.right - box.left);
+    var height = box.height || (box.bottom - box.top);
+    var top, left, right, bottom;
+    winHeight = this.window().height;
+    winWidth = this.window().width;
+    top = box.top;
+    left = box.left;
+    right = winWidth - box.right;
+    bottom = winHeight - box.bottom;
+    return {
+      width: width,
+      height: height,
+      top: top,
+      left: left,
+      right: right,
+      bottom: bottom
+    };
+  },
+  client: function(ele){
+    return {
+      width: ele.clientWidth,
+      height: ele.clientHeight
+    };
+  },
+  scroll: function(ele){
+    var scrollX, scrollY;
+    if(w.pageXOffset != null){
+      scrollX = w.pageXOffset;
+      scrollY = w.pageYOffset;
+    } else if(document.compatMode == 'CSS1Compat'){
+      scrollX = d.documentElement.scrollLeft;
+      scrollY = d.documentElement.scrollTop;
+    } else {
+      scrollX = d.body.scrollLeft;
+      scrollY = d.body.scrollTop;
+    };
+    return {
+      scrollX: scrollX,
+      scrollY: scrollY
+    }
+  }
+}
